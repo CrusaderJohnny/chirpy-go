ad9903b291404cc13a5d0730557e24760c783ea3
CrusaderJohnny <17ark.leonardis@gmail.com>
2026-02-05 11:43:22 -0700

    added postgresql database, created users table, added method to add user and delete all users, added functionality to handle requests to create a new user as well as delete, added validation to deleting users request to prevent unauthorized access

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/CrusaderJohnny/chirpy-go.git/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 )
 
 const filepathRoot = "."
@@ -14,6 +21,15 @@ const port = "8080"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	Id         uuid.UUID `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Email      string    `json:"email"`
 }
 
 type returnVals struct {
@@ -35,7 +51,26 @@ var badWords = map[string]struct{}{
 }
 
 func main() {
-	apiCfg := apiConfig{}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging database: %s", err)
+	}
+	log.Println("Successfully connected to database")
+	platform := os.Getenv("PLATFORM")
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		db:       dbQueries,
+		platform: platform,
+	}
 	mux := http.NewServeMux()
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInt(fileServerHandler))
@@ -43,12 +78,13 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerJson)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerAddUser)
 	srv := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Println("Starting server on port 8080")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -75,10 +111,18 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Platform is only available for admins")
+		return
+	}
+	err := cfg.db.DeleteUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
 }
 
 func (cfg *apiConfig) handlerJson(w http.ResponseWriter, r *http.Request) {
@@ -133,4 +177,37 @@ func cleanChirps(body string) string {
 		}
 	}
 	return strings.Join(words, " ")
+}
+
+func (cfg *apiConfig) handlerAddUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := User{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create user")
+		return
+	}
+	respondWithUser(w, http.StatusCreated, User{
+		Id:         user.ID,
+		Created_at: user.CreatedAt,
+		Updated_at: user.UpdatedAt,
+		Email:      user.Email,
+	})
+}
+
+func respondWithUser(w http.ResponseWriter, code int, user User) {
+	data, err := json.Marshal(user)
+	if err != nil {
+		log.Printf("Error marshalling json: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
 }
